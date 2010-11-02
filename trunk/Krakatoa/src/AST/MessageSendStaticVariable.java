1ba3966713abfc70261cfd081e280fddc46722e3
katharinaCG <katharinaCG@480d8ad6-f5f6-88c9-d33c-c93d34f687ab>
2010-11-02 22:21:20 +0000

    soh tem um erro sobrando >.<

---
+++ trunk/Krakatoa/src/AST/MessageSendStaticVariable.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *Integrantes
+ *
+ *      Bruno Antunes da Silva           317292
+ *      Katharina Carrapatoso Garcia     317144
+ *
+ */
+
+package AST;
+
+
+public class MessageSendStaticVariable extends MessageSend {
+
+
+    public void genC( PW pw, boolean putParenthesis ) {
+    }
+
+    public void genKrakatoa(PW pw, boolean putParenthesis){
+
+    }
+
+
+    public MessageSendStaticVariable(Variable v, ClassDec cl) {
+        this.v = v;
+        this.cl = cl;
+    }
+
+    public Type getType(){
+
+        return v.getType();
+    }
+
+    private Variable v;
+    private ClassDec cl;
+
+}
